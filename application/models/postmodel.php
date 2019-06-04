742a36fa998ffc91887fc6eb1fb861cd6c6c4022
nicolas-camacho <ncamacho1@cuc.edu.co>
2019-06-04 14:54:00 -0500

    basic model and database configuration

---
+++ application/models/postmodel.php
@@ -0,0 +1,27 @@
+<?php if( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Postmodel extends CI_Model
+{
+    var $table = "post";
+    function __construct()
+    {
+        parent::__construct();
+    }
+    function getAll()
+    {
+        $q = $this->db->get($this->table);
+        if ($q->num_rows() > 0) {
+            return $q->$result();
+        }
+        return array();
+    }
+    function add($data)
+    {
+        $this->db->insert($this->table,$data);
+    }
+    function update($data,$id)
+    {
+        $this->db->where("id",$id);
+        $this->db->delete($this->table);
+    }
+}
